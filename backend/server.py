1bc9775bfa2978630284ad42f95f513f7202ed1c
skj_og <95894160+Jha-Shubham19@users.noreply.github.com>
2025-01-04 10:27:09 +0530

    all origin

--- backend/server.py
+++ backend/server.py
@@ -56,7 +56,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Add your frontend origin
+    allow_origins=["*"],  # Add your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
